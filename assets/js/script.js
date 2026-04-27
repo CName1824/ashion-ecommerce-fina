8ebd104bbdc34a69109f1d781a18e410c3438b0e
TrongNghiaa-glitch <tptrongnghiaa2709@gmail.com>
2026-04-28 02:43:21 +0700

    Add countdown discount

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const DiscountDate = new Date("May 28, 2026 00:00:00").getTime();
+
+const x = setInterval(function() {
+
+  const now = new Date().getTime();
+
+  const distance = DiscountDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("mins").innerHTML = mins;
+  document.getElementById("secs").innerHTML = secs;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "ĐÃ ĐẾN GIỜ!";
+  }
+}, 1000);
