cd83231caf284f9bdc4479ea91b9d55db4120d34
CNane1824 <22t1020051@husc.edu.vn>
2026-04-30 15:06:14 +0700

    feat:gần hoàn thiện Product Detail và Shop Cart

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,25 @@ $('.filter-control li').on('click', function () {
     $('.filter-control li').removeClass('active'); // Xóa gạch đỏ ở nút cũ
     $(this).addClass('active'); // Thêm gạch đỏ vào nút vừa bấm
 });
+
+$('.size-btn label').on('click', function () {
+    $('.size-btn label').removeClass('active');
+    $(this).addClass('active');
+})
+
+var proQty = $('.pro-qty');
+proQty.on('click', '.qtybtn', function () {
+    var $button = $(this);
+    var oldValue = $button.parent().find('input').val();
+    if ($button.hasClass('inc')) {
+        var newVal = parseFloat(oldValue) + 1;
+    } else {
+        // Don't allow decrementing below zero
+        if (oldValue > 0) {
+            var newVal = parseFloat(oldValue) - 1;
+        } else {
+            newVal = 0;
+        }
+    }
+    $button.parent().find('input').val(newVal);
+});
