3dde4a85c3eb893b2c028000207ca3227335c255
CNane1824 <22t1020051@husc.edu.vn>
2026-04-30 23:21:36 +0700

    Tân gửi phần Footer cho trang chủ

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,24 @@ $('.filter-control li').on('click', function () {
     $('.filter-control li').removeClass('active'); // Xóa gạch đỏ ở nút cũ
     $(this).addClass('active'); // Thêm gạch đỏ vào nút vừa bấm
 });
+$(document).ready(function () {
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
 
+    var bannerSlider = $(".banner_slider");
+
+    bannerSlider.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 1500,
+        dots: true,
+        nav: false,
+    });
+});
 $('.size-btn label').on('click', function () {
     $('.size-btn label').removeClass('active');
     $(this).addClass('active');
