1b848e7d39c6aeb2188a76c58918bfd1bdcb9d39
CNane1824 <22t1020051@husc.edu.vn>
2026-05-08 10:10:30 +0700

    feat: hoàn thiện phần slider trong product-detail

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,9 @@
-$('.set-bg').each(function () {
-    var bg = $(this).data('setbg');
-    $(this).css('background-image', 'url(' + bg + ')');
-});
 
-$('.filter-control li').on('click', function () {
-    $('.filter-control li').removeClass('active'); // Xóa gạch đỏ ở nút cũ
-    $(this).addClass('active'); // Thêm gạch đỏ vào nút vừa bấm
-});
 $(document).ready(function () {
     $('.set-bg').each(function () {
         var bg = $(this).data('setbg');
         $(this).css('background-image', 'url(' + bg + ')');
-    });
+    }); 
 
     var bannerSlider = $(".banner_slider");
 
@@ -24,11 +16,16 @@ $(document).ready(function () {
         dots: true,
         nav: false,
     });
-});
-$('.size-btn label').on('click', function () {
-    $('.size-btn label').removeClass('active');
-    $(this).addClass('active');
-})
+
+    $('.filter-control li').on('click', function () {
+        $('.filter-control li').removeClass('active'); // Xóa gạch đỏ ở nút cũ
+        $(this).addClass('active'); // Thêm gạch đỏ vào nút vừa bấm
+    });
+    $('.size-btn label').on('click', function () {
+        $('.size-btn label').removeClass('active');
+        $(this).addClass('active');
+    });
+    
 
     var proQty = $('.pro-qty');
     proQty.on('click', '.qtybtn', function () {
@@ -46,21 +43,30 @@ $('.size-btn label').on('click', function () {
         }
         $button.parent().find('input').val(newVal);
     });
-    $(document).ready(function () {
-        $('.set-bg').each(function () {
-            var bg = $(this).data('setbg');
-            $(this).css('background-image', 'url(' + bg + ')');
-        });
 
-        var bannerSlider = $(".banner_slider");
 
-        bannerSlider.owlCarousel({
-            items: 1,
-            loop: true,
-            autoplay: true,
-            autoplayTimeout: 5000,
-            smartSpeed: 1500,
-            dots: true,
-            nav: false,
-        });
+    $(".product-detail-pic-slider").owlCarousel({
+        loop: false,
+        margin: 0,
+        items: 1,
+        dots: false,
+        nav: true,
+        navText: ["<i class='arrow_carrot-left'></i>", "<i class='arrow_carrot-right'></i>"],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: false,
+        mouseDrag: false,
+        startPosition: 'URLHash'
     });
+    $('.product-thumb a').on('click', function (e) {
+        $('.product-thumb a').removeClass('active');
+        $(this).addClass('active');
+    });
+    var product_slider = $(".product-detail-pic-slider");
+    product_slider.on('changed.owl.carousel', function (event) {
+        var index = event.item.index;
+        var all_thumbs = $('.product-thumb a');
+        all_thumbs.removeClass('active');
+        all_thumbs.eq(index).addClass('active');
+    });
+});
