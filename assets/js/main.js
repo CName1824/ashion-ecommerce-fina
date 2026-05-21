e39ec310f13659f4876bb3f32b31e3ea8a8b20e4
CNane1824 <22t1020051@husc.edu.vn>
2026-05-21 20:06:37 +0700

    feat: hoàn thiện responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
         $(this).css('background-image', 'url(' + bg + ')');
     }); 
 
+
     var bannerSlider = $(".banner_slider");
 
     bannerSlider.owlCarousel({
@@ -46,42 +47,16 @@ $(document).ready(function () {
 
 
     $(".product-detail-pic-slider").owlCarousel({
-        // 1. Vòng lặp: Nếu để 'true', khi đến ảnh cuối nó sẽ quay lại ảnh đầu. 
-        // Ở đây để 'false' nghĩa là đến ảnh cuối cùng sẽ dừng lại.
         loop: false,
-
-        // 2. Khoảng cách giữa các ảnh: 0 pixel (các ảnh nằm sát nhau).
         margin: 0,
-
-        // 3. Số lượng ảnh hiển thị cùng một lúc trên màn hình: 1 ảnh.
         items: 1,
-
-        // 4. Các chấm tròn nhỏ ở dưới slide: 'false' là ẩn chúng đi.
         dots: false,
-
-        // 5. Thanh điều hướng (Nút Next/Prev): 'true' là hiển thị hai nút mũi tên.
         nav: true,
-
-        // 6. Nội dung hiển thị trong nút điều hướng: 
-        // Ở đây bạn đang dùng icon mũi tên từ thư viện Elegant Icons.
         navText: ["<i class='arrow_carrot-left'></i>", "<i class='arrow_carrot-right'></i>"],
-
-        // 7. Tốc độ chuyển ảnh: 1200ms (tương đương 1.2 giây), giúp hiệu ứng mượt mà hơn.
         smartSpeed: 1200,
-
-        // 8. Tự động thay đổi chiều cao slide theo ảnh: 
-        // 'false' nghĩa là các ảnh nên có cùng kích thước để slider ổn định.
         autoHeight: false,
-
-        // 9. Tự động chạy slide: 'false' là người dùng phải tự bấm mới chuyển ảnh.
         autoplay: false,
-
-        // 10. Kéo bằng chuột: 'false' là khóa tính năng dùng chuột vuốt/kéo ảnh.
         mouseDrag: false,
-
-        // 11. Vị trí bắt đầu của slide dựa trên URL: 
-        // Thường dùng khi bạn click vào ảnh nhỏ (Thumbnail) có gắn mã ID (#) 
-        // thì ảnh to tương ứng sẽ hiện lên ngay lập tức.
         startPosition: 'URLHash'
     });
     $('.product-thumb a').on('click', function (e) {
