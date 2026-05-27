f9dc9d2b38da359f4484ee092119cf85da3ddb7a
TrongNghiaa-glitch <tptrongnghiaa2709@gmail.com>
2026-05-27 08:55:45 +0700

    Fix console

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@
 $(document).ready(function () {
     $('.set-bg').each(function () {
         var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
+        if (bg) {
+            $(this).css('background-image', 'url(' + bg + ')');
+        }
     }); 
 
     var bannerSlider = $(".banner_slider");
